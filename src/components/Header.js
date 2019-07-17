8692ac461b74394f533188bef0300f2bca3197a8
João Felipe <jo_5919@hotmail.com>
2019-07-17 05:20:53 -0300

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+//import logo from '../assets';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="https://i.imgur.com/KDIDiSE.png" />
+        <div>
+          <span>Meu perfil</span>
+          <i className="material-icons">account_circle</i>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
